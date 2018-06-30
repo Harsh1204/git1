8dc461fb2adf4135a02dea696fca0198b237a3a5
Harsh1204 <38696854+Harsh1204@users.noreply.github.com>
2018-06-30 04:46:35 -0700

    Update LoginPage.java

--- seleframework1/src/pompages/LoginPage.java
+++ seleframework1/src/pompages/LoginPage.java
@@ -11,7 +11,7 @@ public class LoginPage {
 	@FindBy(name="pwd")
 	WebElement pdTb;
 	@FindBy(id="loginButton")
-	WebElement btn;
+	WebElement btn;//init
 
 	public LoginPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
